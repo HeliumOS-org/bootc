52f1026c144ba0acb4cfba82ce04eeb841fdd30d
Isaac Beverly <imbev@protonmail.com>
2024-09-15 14:52:52 -0500

    Add docker

--- 9/apps.sh
+++ 9/apps.sh
@@ -7,3 +7,11 @@ set -e
 dnf install -y \
     distrobox \
     git
+
+
+dnf install -y \
+    docker-ce \
+    docker-ce-cli \
+    containerd.io \
+    docker-buildx-plugin \
+    docker-compose-plugin
