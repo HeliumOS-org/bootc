643ab28ae3bb425d3cd40dd01c14d71980818778
Isaac Beverly <imbev@protonmail.com>
2024-09-16 08:45:10 -0500

    Revert "Add docker"
    This reverts commit 52f1026c144ba0acb4cfba82ce04eeb841fdd30d.

--- 9/apps.sh
+++ 9/apps.sh
@@ -7,11 +7,3 @@ set -e
 dnf install -y \
     distrobox \
     git
-
-
-dnf install -y \
-    docker-ce \
-    docker-ce-cli \
-    containerd.io \
-    docker-buildx-plugin \
-    docker-compose-plugin
--- 9/repos.sh
+++ 9/repos.sh
@@ -12,10 +12,6 @@ dnf config-manager --set-enabled \
     crb
 
 
-dnf config-manager --add-repo \
-    https://download.docker.com/linux/rhel/docker-ce.repo
-
-
 dnf config-manager --save \
     --setopt=exclude=PackageKit,PackageKit-command-not-found,rootfiles,firefox,totem,gnome-tour
 
