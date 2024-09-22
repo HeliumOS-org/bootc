6fc99100dee6d5558f3ac17b610b15f90f4c90e7
Isaac Beverly <imbev@protonmail.com>
2024-09-22 14:11:18 -0500

    Don't rebuild initramfs for Nvidia

--- 9/kernel-nvidia.sh
+++ 9/kernel-nvidia.sh
@@ -4,20 +4,11 @@
 set -e
 
 
-dnf remove -y \
-    kernel-core
-
-
 dnf install -y \
-    kernel-core \
-    kernel-devel
+    kernel-devel \
+    kernel-headers
 
 
 dnf module install -y \
     nvidia-driver:latest-dkms
 
-
-kver=$(cd /usr/lib/modules && echo * | awk '{print $1}')
-
-
-dracut -vf /usr/lib/modules/$kver/initramfs.img $kver
