d1c316615e32127d0d83b997d44e461fdf1f4c7f
Isaac Beverly <imbev@protonmail.com>
2024-10-05 16:17:07 -0500

    Manually install ocl-icd

--- 9/apps.sh
+++ 9/apps.sh
@@ -4,13 +4,17 @@
 set -e
 
 
+# https://codeberg.org/HeliumOS/bugs/issues/2
+dnf install -y \
+    https://repo.almalinux.org/almalinux/9/AppStream/x86_64/os/Packages/ocl-icd-2.2.13-4.el9.x86_64.rpm
+
+
 dnf install -y \
     distrobox \
     chromium \
-    ocl-icd \ # https://codeberg.org/HeliumOS/bugs/issues/2
     git
 
-
+    
 dnf install -y \
     https://kojipkgs.fedoraproject.org//packages/bootc-gtk/0.3/1.el9/noarch/bootc-gtk-0.3-1.el9.noarch.rpm
 
