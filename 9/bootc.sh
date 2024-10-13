b1b8ca19ff29db523fd1e5253494e92849e79218
imbev <imbev@noreply.codeberg.org>
2024-10-13 19:14:24 +0000

    Disable atd.service

--- 9/bootc.sh
+++ 9/bootc.sh
@@ -6,3 +6,6 @@ set -e
 
 sed -i 's,ExecStart=/usr/bin/bootc update --apply --quiet,ExecStart=/usr/bin/bootc update --quiet,g' \
     /usr/lib/systemd/system/bootc-fetch-apply-updates.service
+
+
+systemctl disable atd.service
